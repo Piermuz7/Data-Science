3e51079b176f71e4824240070f37ffe9b6fec583
Piermuz7 <piermichele.rosati@gmail.com>
2023-11-09 18:13:18 +0100

    [add] Variable transformation

--- project-exam/assignment1-regression/preprocessing.R
+++ project-exam/assignment1-regression/preprocessing.R
@@ -1,11 +1,33 @@
+# Packages
+#install.packages("fastDummies")
+#install.packages("readr")
+library(fastDummies)
+library(readr)
+
+#### Data Preprocessing ####
+
 # set the working directory
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # read the csv
-data <- read.csv("LCdata.csv", sep = ";", fileEncoding="UTF-8")
+lcdata <- read.csv("LCdata.csv", sep = ";", fileEncoding="UTF-8")
 print(names(data))
-data <- subset(data, select = -c(collection_recovery_fee, installment, issue_d,
+lcdata <- subset(lcdata, select = -c(collection_recovery_fee, installment, issue_d,
                                  last_pymnt_amnt, last_pymnt_d, loan_status,
                                  next_pymnt_d, out_prncp, out_prncp_inv,
                                  pymnt_plan, recoveries, term, total_pymnt,
                                  total_pymnt_inv, title, desc, url, member_id, policy_code, emp_title, pymnt_plan,id))
+
+# TODO: remove NAs values
+
+# Identify categorical columns
+cat_col_names <- names(lcdata)[sapply(lcdata, is.character)]
+
+# One-Hot encode all categorical columns
+lc_data <- dummy_cols(lcdata, select_columns = cat_col_names, remove_selected_columns = TRUE)
+
+# linear regression on ALL pre-processed data attributes:
+lm.fit <- lm(int_rate~., data=lc_data)
+
+# calculate RMSE
+sqrt(mean(lm.fit$residuals^2))
