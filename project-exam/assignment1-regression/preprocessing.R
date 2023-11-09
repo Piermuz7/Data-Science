962e720d4afcaab28c844a45671fdc3eebff7365
Piermuz7 <piermichele.rosati@gmail.com>
2023-11-09 18:15:39 +0100

    [edit] from lcdata to lc_data

--- project-exam/assignment1-regression/preprocessing.R
+++ project-exam/assignment1-regression/preprocessing.R
@@ -10,9 +10,8 @@ library(readr)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # read the csv
-lcdata <- read.csv("LCdata.csv", sep = ";", fileEncoding="UTF-8")
-print(names(data))
-lcdata <- subset(lcdata, select = -c(collection_recovery_fee, installment, issue_d,
+lc_data <- read.csv("LCdata.csv", sep = ";", fileEncoding="UTF-8")
+lc_data <- subset(lc_data, select = -c(collection_recovery_fee, installment, issue_d,
                                  last_pymnt_amnt, last_pymnt_d, loan_status,
                                  next_pymnt_d, out_prncp, out_prncp_inv,
                                  pymnt_plan, recoveries, term, total_pymnt,
@@ -21,10 +20,10 @@ lcdata <- subset(lcdata, select = -c(collection_recovery_fee, installment, issue
 # TODO: remove NAs values
 
 # Identify categorical columns
-cat_col_names <- names(lcdata)[sapply(lcdata, is.character)]
+cat_col_names <- names(lc_data)[sapply(lc_data, is.character)]
 
 # One-Hot encode all categorical columns
-lc_data <- dummy_cols(lcdata, select_columns = cat_col_names, remove_selected_columns = TRUE)
+lc_data <- dummy_cols(lc_data, select_columns = cat_col_names, remove_selected_columns = TRUE)
 
 # linear regression on ALL pre-processed data attributes:
 lm.fit <- lm(int_rate~., data=lc_data)
