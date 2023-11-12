1cafb6831f5614c861d065cb110b30cfc00a1585
xPieroxk <piero.hierro1@gmail.com>
2023-11-12 21:35:21 +0100

    updated preprocessing.R

--- project-exam/assignment1-regression/preprocessing.R
+++ project-exam/assignment1-regression/preprocessing.R
@@ -1,8 +1,12 @@
 # Packages
 #install.packages("fastDummies")
 #install.packages("readr")
+#install.packages("dplyr")
+#install.packages("caret")
 library(fastDummies)
 library(readr)
+library(dplyr)
+library(caret)
 
 #### Data Preprocessing ####
 
@@ -10,23 +14,35 @@ library(readr)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # read the csv
-lc_data <- read.csv("LCdata.csv", sep = ";", fileEncoding="UTF-8")
+lc_data <- read_delim("LCdata.csv")
+# remove unnecessary columns
 lc_data <- subset(lc_data, select = -c(collection_recovery_fee, installment, issue_d,
-                                 last_pymnt_amnt, last_pymnt_d, loan_status,
-                                 next_pymnt_d, out_prncp, out_prncp_inv,
-                                 pymnt_plan, recoveries, term, total_pymnt,
-                                 total_pymnt_inv, title, desc, url, member_id, policy_code, emp_title, pymnt_plan,id))
+                                       last_pymnt_amnt, last_pymnt_d, loan_status,
+                                       next_pymnt_d, out_prncp, out_prncp_inv,
+                                       pymnt_plan, recoveries, term, total_pymnt,
+                                       total_pymnt_inv, title, desc, url, member_id, policy_code, emp_title, pymnt_plan, id))
+# remove all NAs values
+lc_data <- lc_data[complete.cases(lc_data),]
 
-# TODO: remove NAs values
-
-# Identify categorical columns
+# identify categorical columns
 cat_col_names <- names(lc_data)[sapply(lc_data, is.character)]
 
-# One-Hot encode all categorical columns
-lc_data <- dummy_cols(lc_data, select_columns = cat_col_names, remove_selected_columns = TRUE)
+# subset data for preprocessing (exclude categorical variables)
+lc_data_num <- lc_data[, setdiff(names(lc_data), cat_col_names), drop = FALSE]
+
+# first standardize and then normalize data
+preprocess <- preProcess(lc_data_num, method = c("center", "scale"))
+
+# Print summary statistics of the preprocessed data
+print(summary(predict(preprocess, newdata = lc_data_num)))
+
+
+# one-hot encode all categorical columns
+#lc_data <- dummy_cols(lc_data, select_columns = cat_col_names, remove_selected_columns = TRUE)
+
 
 # linear regression on ALL pre-processed data attributes:
-lm.fit <- lm(int_rate~., data=lc_data)
+#lm.fit <- lm(int_rate~., data=lc_data)
 
 # calculate RMSE
-sqrt(mean(lm.fit$residuals^2))
+#sqrt(mean(lm.fit$residuals^2))
